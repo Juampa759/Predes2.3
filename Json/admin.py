99fb87fe2ee36e604c3f96029dec0ab2a1438cd6
juampa759 <juampa759@gmail.com>
2021-05-26 21:44:52 -0500

    version admin respuesta

--- Json/admin.py
+++ Json/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from Json.models import person, pregunta, RespuestaUsu
 class personAdmin(admin.ModelAdmin):
-    list_display=("User","pregunta","score")
+    list_display=("User_id","pregunta","score")
 
 class pregunasAdmin(admin.ModelAdmin):
     list_display=("pregunta","resA","resB","resC","resD")
 
 class respuestaAdmin(admin.ModelAdmin):
-    list_display=("User","pregunta","respuesta")
+    list_display=("User_id","Pregunta_id","respuesta")
 
 
 # Register your models here.
